7599c045730ab729f79b4f0cd0dd64125318d68b
Divyanshu <38465652+badbola@users.noreply.github.com>
2020-05-27 21:54:31 +0530

    Second commit:Added controller

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port= 8000;
 
+
+
+app.use('/', require('./routes'));
+
 app.listen(port, function(err){
     if(err){ console.log(`Erro in connectinf ${port} due to ${err} error`);}
 
