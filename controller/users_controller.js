67ab435faecea7a3780a629f273f9bd02762190e
Divyanshu <38465652+badbola@users.noreply.github.com>
2020-05-29 22:45:44 +0530

    Sixth commit: added authentication for sign in and updated profile page

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,9 +1,20 @@
 const User = require('../model/user');
 
 module.exports.profile = function(req,res){
-    return res.render('profile', {
-        title: 'Profile page'
-    });
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err,user){
+            if(user){
+                return res.render('profile', {
+                    title: "User Profile",
+                    user: user
+                })
+            }else{
+                return res.redirect('/users/signin');
+            }
+        })
+    }else{
+        return res.redirect('/users/signin');
+    }
 }
 
 module.exports.signup = function(req,res){
@@ -17,6 +28,7 @@ module.exports.signin = function(req,res){
         title: 'Soshell | Signin'
     })
 }
+
 
 module.exports.create = function(req,res){
     if(req.body.password != req.body.confirm_password){
@@ -37,5 +49,19 @@ module.exports.create = function(req,res){
     });
 }
 module.exports.createSession = function(req,res){
-    //todo later
+    User.findOne({email: req.body.email}, function(err,user){
+        if(err){ console.log('Error in signing in'); return; }
+
+        if(user){
+            if(user.password != req.body.password){
+                console.log('Invalid Password');
+                return res.redirect('back')
+            }
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+        }else{
+            console.log('Invalid User');
+            return res.redirect('back');
+        }
+    });
 }
